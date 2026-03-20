66fad592699d4f00f1db0285a00dff18c64e87e5
Jeyasurya-tk <jeyasuryatk@gmail.com>
2026-03-20 17:00:20 +0530

    Change server binding from 127.0.0.1 to 0.0.0.0

--- server/index.ts
+++ server/index.ts
@@ -132,7 +132,7 @@ app.use((req, res, next) => {
   });
 
   const port = parseInt(process.env.PORT || "5000", 10);
-  httpServer.listen(port, "127.0.0.1", () => {
+  httpServer.listen(port, "0.0.0.0", () => {
     log(`backend serving on port ${port}`);
   });
 })();
